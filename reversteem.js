94dec21b58c1eb11cf2a659f5696da6568bf9cc6
Reversi Steem <reversteem@hotmail.com>
2022-09-24 06:54:05 +0900

    Fixed first test

--- reversteem.js
+++ reversteem.js
@@ -1,20 +1,21 @@
 "use strict";
 
 const steem = require("steem");
-steem.api.setOptions({
-  url: "https://api.steemit.com",
-  address_prefix: 'TST',
-  chain_id: '46d82ab7d8db682eb1959aed0ada039a6d49afa1602491f93dde9cac3e8e6c32',
-  useTestNet: true,
-});
+//steem.api.setOptions({
+  //url: "https://api.steemit.com",
+  //address_prefix: 'TST',
+  //chain_id: '46d82ab7d8db682eb1959aed0ada039a6d49afa1602491f93dde9cac3e8e6c32',
+  //useTestNet: true,
+//});
 
 const fetchAccounts = (accounts) => {
-  //debug('<fetchAccounts/>');				
+  console.debug('<fetchAccounts/>');				
   return new Promise((resolve, reject) => {
     steem.api.getAccounts(accounts, async (err, result) => {
       if (err) {
         reject(err);
       } else {
+        console.debug(`<fetchAccounts ${result}/>`);				
         resolve(result);
       }
     });
