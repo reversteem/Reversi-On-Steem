2aa2dba4228479250abacbfd0f3c443898bfe412
Reversi Steem <reversteem@hotmail.com>
2022-09-24 05:38:28 +0900

    Added steem-js and its first test

--- __tests__/reversteem.test.js
+++ __tests__/reversteem.test.js
@@ -1,3 +1,10 @@
 "use strict";
 
-test('it works because nothing i do', () => {})
+const reversteem = require("reversteem");
+
+describe("Account tests", () => {
+ test('fetching "reversteem" account should return the correct key_auths', async () => {
+   const expected = JSON.stringify([["STM7XbJ15F9AYVtEUP5Gm1wTjErvEYofnCFqLt3on3ojkF76aU36L", 1]]);
+   expect(await reversteem.fetchAccounts(["reversteem"]).key_auths).toBe();
+ });
+})
--- reversteem.js
+++ reversteem.js
@@ -1,3 +1,26 @@
 "use strict";
 
-test('it works because nothing i do', () => {})
+const steem = require("steem");
+steem.api.setOptions({
+  url: "https://api.steemit.com",
+  address_prefix: 'TST',
+  chain_id: '46d82ab7d8db682eb1959aed0ada039a6d49afa1602491f93dde9cac3e8e6c32',
+  useTestNet: true,
+});
+
+const fetchAccounts = (accounts) => {
+  //debug('<fetchAccounts/>');				
+  return new Promise((resolve, reject) => {
+    steem.api.getAccounts(accounts, async (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+module.exports = {
+  fetchAccounts
+};
