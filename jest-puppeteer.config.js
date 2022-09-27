aaed4af8b07a7d452e844ec039c0d356f1129a16
Reversi Steem <reversteem@hotmail.com>
2022-09-27 22:58:37 +0900

    Changed chromedrver to /usr/bin/google-chrome

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   launch: {
 //    executablePath: "/usr/bin/chromium-browser"
-    executablePath: "/usr/local/bin/chromedriver"
+    executablePath: "/usr/bin/google-chrome"
   }
 }
